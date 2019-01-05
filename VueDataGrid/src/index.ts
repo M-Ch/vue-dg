9d837d7ab93cc547bf5882b37ee26bec26e3742c
marcin-chwalek <m.chw@outlook.com>
2019-01-05 19:21:11 +0100

    Installable plugin definition

--- VueDataGrid/src/index.ts
+++ VueDataGrid/src/index.ts
@@ -1,12 +1,19 @@
-import Vue, { VueConstructor } from "vue";
+import { VueConstructor } from "vue";
 import DataGrid from "./components/DataGrid";
 import DataColumn from "./components/DataColumn";
+import { addType, setLanguage } from "./Config";
 
 const components: {[key: string]: VueConstructor} = {
    DataGrid,
    DataColumn,
 };
 
-Object.keys(components).forEach(i => Vue.component(i, components[i]));
+const plugin = {
+   install(vue: VueConstructor) {
+      Object.keys(components).forEach(i => vue.component(i, components[i]));
+   },
+   addType,
+   setLanguage
+};
 
-export default components;
+export default plugin;
--- VueDataGrid/src/main.ts
+++ VueDataGrid/src/main.ts
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import SampleGrid from "./SampleGrid.vue";
+import DataGrid from "./index";
+
+DataGrid.addType("customType", v => (""+v).toUpperCase());
+Vue.use(DataGrid);
 
 // tslint:disable-next-line:no-unused-expression
-
 new Vue({
    render: h => h(SampleGrid)
- }).$mount("#test")
+ }).$mount("#test");
