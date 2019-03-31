9191c82014f6804578fe09c1d319bf6a6dead656
marcin-chwalek <m.chw@outlook.com>
2019-03-31 21:41:08 +0200

    Removed mvc sample project

--- VueDataGrid/build.cmd
+++
@@ -1,1 +0,0 @@
-npm run bundle && xcopy dist\dataGrid.umd.js ..\VueDataGrid.Sample\Scripts\dataGrid.umd.js /Y && xcopy dist\dataGrid.css ..\VueDataGrid.Sample\Scripts\dataGrid.css /Y && xcopy dist\dataGrid.umd.js.map ..\VueDataGrid.Sample\Scripts\dataGrid.umd.js.map /Y
